6959cc5480672818ac5fa0d073420e42cbee7b63
Nicolas Tomatis <nicolas.tomatis.f@gmail.com>
2019-06-18 16:09:46 -0300

    Add images for model inference

--- predictions.py
+++ predictions.py
@@ -1,19 +1,22 @@
+from base64 import b64decode
+from io import BytesIO
+from PIL import Image
 from os import getenv
+from os.path import join
 import boto3
 from dotenv import load_dotenv
 from flask import Flask
 from flask import request
 from flask import json
-import numpy as np
-from sklearn.externals import joblib
-
+from pickle import load
 
 # Load environmental variables
 load_dotenv()
 
 BUCKET_NAME = getenv('BUCKET_NAME')
-MODEL_FILE_NAME = getenv('MODEL_FILE_NAME')
-MODEL_LOCAL_PATH = getenv('MODEL_LOCAL_PATH')
+MODEL_WRAPPER = getenv('MODEL_WRAPPER')
+MODEL_WEIGHTS = getenv('MODEL_WEIGHTS')
+TMP_PATH = getenv('TMP_PATH')
 
 
 app = Flask(__name__)
@@ -23,21 +26,28 @@ app = Flask(__name__)
 def index():
     payload = json.loads(request.get_data().decode('utf-8'))
     prediction = predict(payload['payload'])
-    data = dict()
-    data['data'] = prediction[-1]
+    data = {
+        'class': f"{prediction[0]}",
+        'probability': f"{prediction[1]:.6f}"
+    }
     return json.dumps(data)
 
 
 def load_model():
     s3 = boto3.client('s3')
-    s3.download_file(BUCKET_NAME, MODEL_FILE_NAME, MODEL_LOCAL_PATH)
-    return joblib.load(MODEL_LOCAL_PATH)
+    # Load model wrapper
+    # s3.download_file(BUCKET_NAME, MODEL_WRAPPER, join(TMP_PATH, MODEL_WRAPPER))
+    # s3.download_file(BUCKET_NAME, MODEL_WEIGHTS, join(TMP_PATH, MODEL_WEIGHTS))
+    with open(join(TMP_PATH, MODEL_WRAPPER), 'rb') as filehandler:
+        model = load(filehandler)
+    model.load(join(TMP_PATH, MODEL_WEIGHTS))
+    return model
 
 
 def predict(data):
-    # Process your data, create a dataframe/vector and make your predictions
-    final_formatted_data = np.expand_dims(np.fromstring(data.strip('['), sep=','), axis=0)
-    return load_model().predict(final_formatted_data).tolist()
+    # Load encoded image
+    loaded_image = Image.open(BytesIO(b64decode(data))).convert('RGB')
+    return load_model().predict(loaded_image)
 
 
 if __name__ == "__main__":
